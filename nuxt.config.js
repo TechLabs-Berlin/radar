fad082c92375793aaf9d2969d9ba6dee1cb5990f
Daniel da Rocha <danrocha@gmail.com>
2020-09-20 20:30:14 +0200

    fix axios config

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,15 @@ export default {
     '@nuxtjs/style-resources',
     '@nuxtjs/axios'
   ],
-  axios: {},
+  axios: {
+    // headers: {
+    //   'Access-Control-Allow-Headers': 'Content-Type',
+    //   'Access-Control-Allow-Origin': '*',
+    //   'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST',
+    //   'Access-Control-Allow-Headers':
+    //     'Access-Control-Allow-Origin, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+    // }
+  },
   /*
    ** Build configuration
    */
