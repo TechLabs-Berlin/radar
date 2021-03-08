6ce353a78d9fbf111954632979d7b62c042b0e78
Daniel da Rocha <daniel.darocha@havr.io>
2021-03-08 12:54:05 +0100

    implement index

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,22 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">radar_new</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <main class="h-full bg-gray-50">
+    <div class="container px-4 py-8 mx-auto timeline-grid">
+      <div>
+        <Timeline />
       </div>
+      <div>events</div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style lang="scss" scoped>
+.timeline-grid {
+  display: grid;
+  grid-template-columns: 300px 1fr;
+  grid-gap: 2rem;
 }
 </style>
