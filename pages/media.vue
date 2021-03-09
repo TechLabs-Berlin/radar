3f2b586e88d37377c0f3a3c85b00f5e09e05b062
Daniel da Rocha <daniel.darocha@havr.io>
2021-03-09 08:28:41 +0100

    implement media page

---
+++ pages/media.vue
@@ -0,0 +1,64 @@
+<script>
+import {
+  defineComponent,
+  useFetch,
+  useContext,
+  ref,
+} from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const { $content } = useContext()
+
+    const mediaItems = ref(null)
+
+    const { fetchState } = useFetch(async () => {
+      mediaItems.value = await $content('media').fetch()
+    })
+
+    return { mediaItems, fetchState }
+  },
+})
+</script>
+
+<template>
+  <main>
+    <div class="max-w-2xl px-4 py-8 mx-auto space-y-8">
+      <h1 class="text-4xl font-bold text-center">Media</h1>
+      <template v-if="!fetchState.pending">
+        <template v-if="!mediaItems.items || !mediaItems.items.length">
+          <WrapperContentBox>
+            <p>
+              As the semester progresses, we'll add videos of all our events
+              here.
+            </p>
+          </WrapperContentBox>
+        </template>
+        <template v-else>
+          <WrapperContentBox
+            v-for="media in mediaItems.items"
+            :key="media.title"
+          >
+            <div class="flex justify-between mb-4">
+              <div>
+                <h2 class="text-xl font-bold">{{ media.title }}</h2>
+                <p>{{ media.date }}</p>
+              </div>
+              <div>
+                <a
+                  :href="media.youtubeURL"
+                  target="_blank"
+                  class="flex items-center px-4 py-2 space-x-2 border-2 border-gray-700 rounded-xl hover:border-pink-600 hover:text-pink-600"
+                  ><span><TIcon icon="youtube" class="text-xl" /></span
+                  ><span class="whitespace-nowrap">Watch it on YouTube</span></a
+                >
+              </div>
+            </div>
+            <!-- eslint-disable-next-line vue/no-v-html -->
+            <div class="prose" v-html="$md.render(media.description)" />
+          </WrapperContentBox>
+        </template>
+      </template>
+    </div>
+  </main>
+</template>
