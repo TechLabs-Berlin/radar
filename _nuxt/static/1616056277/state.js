884a2d6943406723720d2db83fc88d9437922652
TechLabs Berlin GitHub Workflow <berlin@techlabs.org>
2021-03-18 08:31:48 +0000

    Add frontend build at 2021-03-18T08:31:48UTC

---
+++ _nuxt/static/1616056277/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616056277",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8e9d3a01"}},globalRefs:{}}}(null,"\u002F"));
