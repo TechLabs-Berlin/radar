d6a1fb00d95aa96019979efd110e67d0a1d742ee
TechLabs Berlin GitHub Workflow <berlin@techlabs.org>
2021-03-20 07:44:32 +0000

    Add frontend build at 2021-03-20T07:44:32UTC

--- _nuxt/static/1616056277/event/project-pitches/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616056277",layout:"default",error:a,serverRendered:true,routePath:"\u002Fevent\u002Fproject-pitches",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8e9d3a01"}},globalRefs:{}}}(null));
