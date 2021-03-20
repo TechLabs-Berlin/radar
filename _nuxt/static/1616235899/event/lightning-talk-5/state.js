f05f933d8e7dab0d844a1c902c43ff67c28f51a5
TechLabs Berlin GitHub Workflow <berlin@techlabs.org>
2021-03-20 10:54:32 +0000

    Add frontend build at 2021-03-20T10:54:32UTC

--- _nuxt/static/1616235899/event/lightning-talk-5/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616235899",layout:"default",error:a,serverRendered:true,routePath:"\u002Fevent\u002Flightning-talk-5",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bc0e82dc"}},globalRefs:{}}}(null));
---
+++ _nuxt/static/1616237633/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616237633",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f40a56a6"}},globalRefs:{}}}(null,"\u002F"));
