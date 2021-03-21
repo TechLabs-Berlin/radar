3694aea3cc13d1ab88a83f4f0ada4e18ae7a8edc
TechLabs Berlin GitHub Workflow <berlin@techlabs.org>
2021-03-21 09:51:25 +0000

    Add frontend build at 2021-03-21T09:51:25UTC

---
+++ _nuxt/static/1616320246/milestone/hatching-phase/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616320246",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmilestone\u002Fhatching-phase",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bc7acf34"}},globalRefs:{}}}(null));
