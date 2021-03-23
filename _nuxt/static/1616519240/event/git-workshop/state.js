a7a3542382ac4946ef4c55789b0654fb60e8053a
TechLabs Berlin GitHub Workflow <berlin@techlabs.org>
2021-03-23 17:24:32 +0000

    Add frontend build at 2021-03-23T17:24:32UTC

--- _nuxt/static/1616519240/event/git-workshop/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616519240",layout:"default",error:a,serverRendered:true,routePath:"\u002Fevent\u002Fgit-workshop",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c102fa71"}},globalRefs:{}}}(null));
---
+++ _nuxt/static/1616520234/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616520234",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ee2b4f4a"}},globalRefs:{}}}(null,"\u002F"));
