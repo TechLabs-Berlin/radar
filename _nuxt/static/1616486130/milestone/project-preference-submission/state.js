a66fac3fa71078a1f3e3395fbdee4e5ddf84188f
TechLabs Berlin GitHub Workflow <berlin@techlabs.org>
2021-03-23 16:20:39 +0000

    Add frontend build at 2021-03-23T16:20:39UTC

--- _nuxt/static/1616486130/milestone/project-preference-submission/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616486130",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmilestone\u002Fproject-preference-submission",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"14511bfa"}},globalRefs:{}}}(null));
---
+++ _nuxt/static/1616516400/event/lightning-talk-6/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616516400",layout:"default",error:a,serverRendered:true,routePath:"\u002Fevent\u002Flightning-talk-6",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"01c00037"}},globalRefs:{}}}(null));
