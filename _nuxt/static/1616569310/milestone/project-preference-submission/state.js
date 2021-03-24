868fcde8b5ba2aefb57b67db29a6dfbf25ab23dd
TechLabs Berlin GitHub Workflow <berlin@techlabs.org>
2021-03-24 07:02:23 +0000

    Add frontend build at 2021-03-24T07:02:23UTC

---
+++ _nuxt/static/1616569310/milestone/project-preference-submission/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616569310",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmilestone\u002Fproject-preference-submission",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b4ac828d"}},globalRefs:{}}}(null));
