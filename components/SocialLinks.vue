c192ad67857c2a7e4f66be373d3914e66bdc69cf
Daniel da Rocha <daniel.darocha@havr.io>
2021-05-06 13:37:17 +0200

    feat: use yml to define social and nav links

--- components/SocialLinks.vue
+++ components/SocialLinks.vue
@@ -1,45 +1,37 @@
 <template>
-  <ul class="flex items-center space-x-4">
-    <li>
-      <a href="#" class="hover:text-pink-600"
-        ><TIcon icon="slack" :class="isSmall ? 'text-xl' : 'text-4xl'"
+  <ul v-if="content" class="flex items-center space-x-4">
+    <li v-for="link in content.socialLinks" :key="link.type">
+      <a :href="link.url" class="hover:text-pink-600"
+        ><TIcon :icon="link.type" :class="isSmall ? 'text-xl' : 'text-4xl'"
       /></a>
-    </li>
-    <li>
-      <a href="#" class="hover:text-pink-600"
-        ><TIcon icon="facebook" :class="isSmall ? 'text-xl' : 'text-4xl'"
-      /></a>
-    </li>
-    <li>
-      <a href="#" class="hover:text-pink-600"
-        ><TIcon icon="instagram" :class="isSmall ? 'text-xl' : 'text-4xl'"
-      /></a>
-    </li>
-    <li v-if="showTechlabsLink">
-      <a
-        href="#"
-        target="_blank"
-        class="flex items-center px-4 py-2 space-x-2 border-2 border-gray-700 rounded-xl hover:border-pink-600 hover:text-pink-600"
-        ><span><TIcon icon="link" class="text-xl" /></span
-        ><span>techlabs.org</span></a
-      >
     </li>
   </ul>
 </template>
 
 <script>
-import { defineComponent } from '@nuxtjs/composition-api'
+import {
+  defineComponent,
+  useFetch,
+  useContext,
+  ref,
+} from '@nuxtjs/composition-api'
+
 export default defineComponent({
   props: {
     isSmall: {
       type: Boolean,
     },
-    showTechlabsLink: {
-      type: Boolean,
-    },
   },
   setup() {
-    return {}
+    const { $content } = useContext()
+
+    const content = ref(null)
+
+    const { fetchState } = useFetch(async () => {
+      content.value = await $content('social-links').fetch()
+    })
+
+    return { content, fetchState }
   },
 })
 </script>
