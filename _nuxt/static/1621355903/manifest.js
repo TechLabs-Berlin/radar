4c112f54990d22f6f4a9ff70e313994deee4adf6
TechLabs Berlin GitHub Workflow <berlin@techlabs.org>
2021-05-18 16:48:36 +0000

    Add frontend build at 2021-05-18T16:48:36UTC

--- _nuxt/static/1621355903/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fnewsletter","\u002Fmedia","\u002Ffaq"]})
---
+++ _nuxt/static/1621356485/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621356485",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"54c7c30e"}},globalRefs:{}}}(null,"\u002F"));
