c45bcce80e61bedfc9d6c8641a1774cfc1bd4ce8
Zubin J <zrjohn@yahoo.com>
2024-08-28 07:35:59 -0400

    chore: update nuxt config for new remote WS

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - Radar | WS23/24 | TechLabs Berlin',
+    titleTemplate: '%s - Radar | WS24/25 | TechLabs Remote Bootcamp',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
